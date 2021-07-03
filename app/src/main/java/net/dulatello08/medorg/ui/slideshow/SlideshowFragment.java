8e67e1a1468c00a92244f9e90592519c295b0c27
Dulat <dulatello08@gmail.com>
2021-07-03 14:19:25 +0600

    Edits

--- app/src/main/java/net/dulatello08/medorg/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/net/dulatello08/medorg/ui/slideshow/SlideshowFragment.java
@@ -17,30 +17,4 @@ import net.dulatello08.medorg.databinding.FragmentSlideshowBinding;
 
 public class SlideshowFragment extends Fragment {
 
-    private SlideshowViewModel slideshowViewModel;
-    private FragmentSlideshowBinding binding;
-
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        slideshowViewModel =
-                new ViewModelProvider(this).get(SlideshowViewModel.class);
-
-        binding = FragmentSlideshowBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        final TextView textView = binding.textSlideshow;
-        slideshowViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
-    }
 }
